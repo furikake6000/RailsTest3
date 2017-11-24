daeb4a783fd651579266d11f838df495126666f3
Furikake <ewave63@keio.jp>
2017-11-24 10:39:34 +0900

    Questのパラメータ設定、同時にto_sメソッドを作成

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -27,7 +27,48 @@ class Quest < ApplicationRecord
       "followed_by_n_user"
     ]
     self.questtype = questtypes.sample
+    @random = Random.new
+    case self.questtype
+    when "follow_user_start_with_x" then
+      self.target = ('あ'..'ん').to_a.shuffle.first
+    when "follow_n_user_contain_x" then
+      self.target = ('あ'..'ん').to_a.shuffle.first
+      self.value = @random.rand(1..3)
+    when "follow_n_user" then
+      self.value = @random.rand(1..5)
+    when "retweet_tweet_start_with_x" then
+      self.target = ('あ'..'ん').to_a.shuffle.first
+    when "retweet_n_tweet" then
+      self.value = @random.rand(3..10)
+    when "tweet_n_tweet" then
+      self.value = @random.rand(3..10)
+    when "tweet_start_with_x" then
+      self.target = ('あ'..'ん').to_a.shuffle.first
+    when "followed_by_n_user" then
+      self.value = @random.rand(1..5)
+    end
     self.last_follower = @last_follower
     self.last_following = @last_following
+  end
+
+  def to_s
+    case self.questtype
+    when "follow_user_start_with_x" then
+      return "「" + self.target + "」 から始まるユーザをフォローしよう！"
+    when "follow_n_user_contain_x" then
+      return "「" + self.target + "」 を含むユーザを" + self.value.to_s + "人フォローしよう！"
+    when "follow_n_user" then
+      return self.value.to_s + "人フォローしよう！"
+    when "retweet_tweet_start_with_x" then
+      return "「" + self.target + "」 から始まるツイートをリツイートしよう！"
+    when "retweet_n_tweet" then
+      return self.value.to_s + "個のツイートをリツイートしよう！"
+    when "tweet_n_tweet" then
+      return self.value.to_s + "ツイートしよう！"
+    when "tweet_start_with_x" then
+      return "「" + self.target + "」 から始まるツイートをしよう！"
+    when "followed_by_n_user" then
+      return self.value.to_s + "人にフォローされよう！"
+    end
   end
 end
