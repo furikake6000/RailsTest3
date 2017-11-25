11197de964bcbc51bed7920acbfcd019ad907f54
Furikake <ewave63@keio.jp>
2017-11-25 21:28:15 +0900

    クエストの進捗をプログレスバーで描画

--- app/assets/javascripts/quest_show.js
+++ app/assets/javascripts/quest_show.js
@@ -7,4 +7,27 @@ function limitdate (limitstr){
                 ('00' + Math.floor(resttime/60%60)).slice(-2) + ":" +
                 ('00' + Math.floor(resttime%60%60)).slice(-2);
   return str;
-}
+};
+
+$.fn.progbar = function(){
+  $(this).drawRect({
+      layer: true,
+      fillStyle: '#000',
+      shadowColor: '#333',
+      shadowBlur: 10,
+      x: 10, y: 10,
+      width: 400,
+      height: 20,
+      mask: true
+    });
+  $(this).drawRect({
+      layer: true,
+      name: 'bar',
+      fillStyle: '#C23685',
+      shadowColor: '#333',
+      shadowBlur: 10,
+      x: -340, y: 10,
+      width: 400,
+      height: 20
+    });
+};
