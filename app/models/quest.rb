45d018c332f025b570c8874da56d7a54699e5719
Furikake <ewave63@keio.jp>
2017-11-25 10:51:07 +0900

    Questsの描画速度に若干の更新

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -81,7 +81,7 @@ class Quest < ApplicationRecord
     begin
       case self.questtype
       when "follow_user_start_with_x" then
-        @friends ||= client.friend_ids({count: 30})
+        @friends ||= client.friend_ids({count: 20})
         @friends.each do |friend|
           break if friend.to_s == self.last_following
           return 100 if client.user(friend).name.start_with?(self.target)
@@ -89,7 +89,7 @@ class Quest < ApplicationRecord
         return 0
       when "follow_n_user_contain_x" then
         count = 0
-        @friends ||= client.friend_ids({count: 30})
+        @friends ||= client.friend_ids({count: 20})
         @friends.each do |friend|
           break if friend.to_s == self.last_following
           count += 100 if client.user(friend).name.include?(self.target)
@@ -97,46 +97,42 @@ class Quest < ApplicationRecord
         return count / self.value
       when "follow_n_user" then
         count = 0
-        @friends ||= client.friend_ids({count: 30})
+        @friends ||= client.friend_ids({count: 20})
         @friends.each do |friend|
           break if friend.to_s == self.last_following
           count += 1
         end
         return count * 100 / self.value
       when "retweet_tweet_start_with_x" then
-        @retweets ||= client.retweeted_by_me({count: 30})
+        @retweets ||= client.retweeted_by_me({count: 30, since_id: self.last_retweet})
         @retweets.each do |tweet|
-          break if tweet.id.to_s == self.last_retweet
           return 100 if tweet.text.start_with?(self.target)
         end
         return 0
       when "retweet_n_tweet" then
         count = 0
-        @retweets ||= client.retweeted_by_me({count: 30})
+        @retweets ||= client.retweeted_by_me({count: 30, since_id: self.last_retweet})
         @retweets.each do |tweet|
-          break if tweet.id.to_s == self.last_retweet
           count += 1
         end
         return count * 100 / self.value
       when "tweet_n_tweet" then
         count = 0
-        @tweets ||= client.user_timeline({user_id: user.twid, count: 30})
+        @tweets ||= client.user_timeline({user_id: user.twid, count: 30, since_id: self.last_tweet})
         @tweets.each do |tweet|
-          break if tweet.id.to_s == self.last_tweet
           count += 1
         end
         return count * 100 / self.value
       when "tweet_start_with_x" then
         count = 0
-        @tweets ||= client.user_timeline({user_id: user.twid, count: 30})
+        @tweets ||= client.user_timeline({user_id: user.twid, count: 30, since_id: self.last_tweet})
         @tweets.each do |tweet|
-          break if tweet.id.to_s == self.last_retweet
           count += 1 if tweet.text.start_with?(self.target)
         end
         return count * 100 / self.value
       when "followed_by_n_user" then
         count = 0
-        @followers ||= client.follower_ids.each
+        @followers ||= client.follower_ids({count: 20})
         @followers.each do |follower|
           break if follower.to_s == self.last_following
           count += 1
