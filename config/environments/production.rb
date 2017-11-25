f64d19709e5bccae9004e85fdd5e2b3075b7d7a5
Furikake <ewave63@keio.jp>
2017-11-25 20:20:00 +0900

    新規jsファイルを正しく使うように

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,6 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Precompile all js codes placed in "assets/javascript"
-  config.assets.precompile += ['*.js']
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
