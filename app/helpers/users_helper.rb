df6568140962fd3256c7f66a28d4a7458be5342d
Furikake <ewave63@keio.jp>
2017-12-04 00:02:34 +0900

    ランキングはTop10のみ取ってくる

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
     @user_tw_account = @client.user(current_user.twid.to_i)
     @quests = @user.quests.all
     @cache = {}
-    @ranking = User.all
+    @ranking = User.limit(10)
     render 'users/show'
   end
 end
