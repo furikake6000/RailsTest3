addd31ec1ace70a821b4c1ef86c400e668f50bff
Furikake <ewave63@keio.jp>
2017-12-05 14:43:11 +0900

    pickAWord関数、ミスってた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def pickAWord
     path = Dir.glob('./app/assets/dict/*')
-    File.open(path.sample, mode = "rt"){|f|
-      return f.readlines.sample
-    }
+    File.open(path.sample, mode = "rt") do |f|
+      return f.readlines.sample.chomp
+    end
   end
 end
