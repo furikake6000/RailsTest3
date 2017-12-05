4d67a0b15b7431aa68da8308b0efb72f94e73826
Furikake <ewave63@keio.jp>
2017-12-05 10:23:09 +0900

    期限切れのクエストを削除するように

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,8 +4,26 @@ module UsersHelper
     @user = current_user
     @user_tw_account = @client.user(current_user.twid.to_i)
     @quests = @user.quests.all
+    quest_reset
     @cache = {}
     @ranking = User.limit(10)
     render 'users/show'
+  end
+
+  #時間切れクエストを削除、更新
+  def quest_reset
+    #履歴削除
+    @quests.each do |quest|
+      if quest.created_at.tomorrow < Time.zone.now
+        quest.destroy
+      end
+    end
+
+    #新規生成
+    (4 - @quests.count).downto(0) do |n|
+      Quest.generate_new(current_user, @client)
+      #@quests更新
+      @quests = @user.quests.all if n==0
+    end
   end
 end
