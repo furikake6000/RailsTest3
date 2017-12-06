3ef7fe46c246432a68d79978539f4290fa022470
Furikake <ewave63@keio.jp>
2017-12-06 17:41:09 +0900

    wordのバリデーションと初期化子

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,12 @@
 class Word < ApplicationRecord
+  include ApplicationHelper
+
   belongs_to :user
   validates :user_id, presence: true
   validates :name, presence: true
 
+  def initialize(attributes = {})
+    super
+    self.name = pickAWord
+  end
 end
