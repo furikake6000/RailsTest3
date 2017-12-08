086a2e7b6bbd74188cd152a5dab5a3e1f7894be4
Furikake <ewave63@keio.jp>
2017-12-08 11:19:43 +0900

    Herokuのエラーを何とかするコミット

--- db/migrate/20171208020729_remove_detector_from_words.rb
+++ db/migrate/20171208020729_remove_detector_from_words.rb
@@ -1,6 +1,5 @@
 class RemoveDetectorFromWords < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :words, :detector, foreign_key: true
     add_column :words, :detectorid, :integer
   end
 end
