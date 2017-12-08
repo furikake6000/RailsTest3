3862236ae6ecba5858aab48a8dc36bd1bc76d3af
Furikake <ewave63@keio.jp>
2017-12-08 10:59:16 +0900

    detectorリレーションを復活

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,7 +2,7 @@ class Word < ApplicationRecord
   include ApplicationHelper
 
   belongs_to :user
-  #belongs_to :detector ,class_name: 'User', foreign_key:'detector_id'
+  belongs_to :detector ,class_name: 'User', foreign_key:'detector_id'
   validates :user_id, presence: true
   validates :name, presence: true
 
