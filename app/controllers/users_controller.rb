6510a25f086248cb2f2441eda5e4cbb969ce010d
Furikake <ewave63@keio.jp>
2017-12-10 13:14:52 +0900

    いっかつこみっと

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    redirect_to root_path if !logged_in?
     @client = client_new
     @user_tw_account = @client.user(params[:id].to_s)
     @user = User.find_by(twid: @user_tw_account.id)
@@ -20,8 +21,12 @@ class UsersController < ApplicationController
       render_404 if @user.nil?
       @word = @user.words.find_by(name: params[:word])
       if !(@word.nil?)
-        @word.detect_by(current_user)
-        @result = "success"
+        if @word.detected
+          @result = "alreadyreported"
+        else
+          @word.detect_by(current_user)
+          @result = "success"
+        end
       else
         current_user.score -= 50
         current_user.save
