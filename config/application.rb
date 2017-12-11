dafd6153eac1a0767d5ec68b7b2d85b4fa8c3a14
Furikake <ewave63@keio.jp>
2017-12-11 23:57:15 +0900

    時刻設定をTokyo(+09:00)に

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RailsTest3
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Set timezone
+    config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
