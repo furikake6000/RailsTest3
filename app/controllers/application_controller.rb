6da3e3f9fc68bd45ad73fd8a76a39e75c454a8a5
Furikake <ewave63@keio.jp>
2017-12-19 09:23:15 +0900

    摘発時のエラーを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
+  #rescue_from Exception, with: :render_500
 
   protect_from_forgery with: :exception
   include ApplicationHelper
